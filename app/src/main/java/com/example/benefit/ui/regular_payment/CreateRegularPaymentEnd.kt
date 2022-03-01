5339cd1b5e672d5fa31095ecedeffe468ae56cb5
jmadaminov <aA1234567>
2022-03-01 19:58:30 +0500

    minor fixes

--- app/src/main/java/com/example/benefit/ui/regular_payment/CreateRegularPaymentEnd.kt
+++ app/src/main/java/com/example/benefit/ui/regular_payment/CreateRegularPaymentEnd.kt
@@ -18,12 +18,16 @@ import com.example.benefit.remote.models.PaynetService
 import com.example.benefit.ui.base.BaseFragment
 import com.example.benefit.ui.main.home.HomeFragment
 import com.example.benefit.util.*
+import com.example.benefit.util.Constants.MONTHS
 import kotlinx.android.synthetic.main.fragment_create_regular_payment_end.*
 import kotlinx.android.synthetic.main.item_card_small.view.*
 import kotlinx.android.synthetic.main.item_paynet_service_field_phone.view.*
 import kotlinx.android.synthetic.main.item_paynet_service_field_spinner.view.*
 import kotlinx.android.synthetic.main.item_paynet_service_field_string.view.*
 import kotlinx.android.synthetic.main.transaction_loading.*
+import kotlinx.android.synthetic.main.transaction_success.*
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import java.text.DecimalFormat
 import javax.inject.Inject
 
@@ -37,6 +41,9 @@ class CreateRegularPaymentEnd @Inject constructor() :
 
     val args by navArgs<CreateRegularPaymentEndArgs>()
     private val viewModel: CreateRegularPaymentViewModel by viewModels()
+
+    var startDate: Long? = null
+    var startTime: Long? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -58,24 +65,57 @@ class CreateRegularPaymentEnd @Inject constructor() :
             findNavController().popBackStack()
         }
 
+        edtDesignation.doOnTextChanged { text, start, before, count ->
+            checkFields()
+        }
+
+        tvDate.setOnClickListener {
+            val dialog = DatePickerFragment()
+            childFragmentManager.setFragmentResultListener(
+                KEY_DATE_SELECTED,
+                viewLifecycleOwner
+            ) { requestKey, result ->
+                startDate = result.getLong(ARG_DATE)
+                val date = DateTime(startDate)
+                tvDate.text = "${date.dayOfMonth} ${MONTHS[AppPrefs.language]!![date.monthOfYear]}"
+                checkFields()
+            }
+            dialog.show(childFragmentManager, "datePicker")
+        }
+
+        tvTime.setOnClickListener {
+            val dialog = TimePickerFragment()
+            childFragmentManager.setFragmentResultListener(
+                KEY_TIME_SELECTED,
+                viewLifecycleOwner
+            ) { requestKey, result ->
+                startTime = result.getLong(ARG_TIME)
+                tvTime.text = DateTimeFormat.forPattern("HH:mm")
+                    .print(DateTime().withTimeAtStartOfDay().plus(startTime!!))
+                checkFields()
+            }
+            dialog.show(childFragmentManager, "timePicker")
+        }
+
         switchSetup.setOnCheckedChangeListener { buttonView, isChecked ->
+            checkFields()
             clSetup.visibility = if (isChecked) View.VISIBLE else View.GONE
         }
 
         tvCreateRegPayment.setOnClickListener {
 
-            val fields = StringBuilder()
-            viewModel.fields.forEach {
-                fields.append("\"${it.name}\":")
-                fields.append("\"${it.userSelection}\",")
+            val fieldsBuilder = StringBuilder()
+            for (field in viewModel.fields) {
+                fieldsBuilder.append("\"${field.name}\":")
+                fieldsBuilder.append("\"${field.userSelection}\",")
             }
 
-            fields.append("\"${viewModel.transferAmountKey}\":")
-            fields.append("\"${edtSum.text.toString().toInt()}\"")
+            fieldsBuilder.append("\"${viewModel.transferAmountKey}\":")
+            fieldsBuilder.append("\"${edtSum.text.toString().toInt()}\"")
 
             viewModel.saveAutoPayment(
-                type = 2,
-                near_date = 0L,
+                type = if (checkOnce.isChecked) 1 else if (checkOnceAWeek.isChecked) 2 else if (checkOnceAMonth.isChecked) 3 else 4,
+                near_date = startDate!! + startTime!!,
                 title = edtDesignation.text.toString(),
                 card_id = if (cardsPager.currentItem == 0) null else viewModel.bftAndMyCardsPair.value!!.second.getProperly()[cardsPager.currentItem - 1].id,
                 provider_id = args.paymentDTO?.providerInfo?.own_id
@@ -84,7 +124,7 @@ class CreateRegularPaymentEnd @Inject constructor() :
                 from_cashback = cardsPager.currentItem == 0,
                 is_notify = switchNotification.isChecked,
                 amount = edtSum.text.toString().toInt(),
-                fields = fields.toString()
+                fields = fieldsBuilder.toString()
             )
 
         }
@@ -103,9 +143,18 @@ class CreateRegularPaymentEnd @Inject constructor() :
             merchant.titleShort?.let { tvBrandName.text = it }
         }
 
-
+        tvCreateRegPayment.myEnabled(false)
     }
 
+    private fun checkFields() {
+        tvCreateRegPayment.myEnabled(
+            startDate != null &&
+                    startTime != null &&
+                    viewModel.fields.allFilled() &&
+                    !edtDesignation.text.isNullOrBlank() &&
+                    switchSetup.isChecked
+        )
+    }
 
     private fun setupCardsPager(bftInfo: BalanceInfo, cardsDTO: List<CardDTO>) {
 
@@ -156,34 +205,6 @@ class CreateRegularPaymentEnd @Inject constructor() :
             viewModel.fields.add(serviceField)
             serviceField.fieldValues is List<*>
             when (serviceField.fieldType) {
-//                FieldType.STRING -> {
-//                    val view =
-//                            layoutInflater.inflate(R.layout.item_paynet_service_field_string, null)
-//                                    .apply {
-//                                        lblTitle.text =
-//                                                if (AppPrefs.language == UZ) serviceField.titleUz else serviceField.titleRu
-//                                        edtInput.doOnTextChanged { text, start, before, count ->
-//                                            text?.let {
-//                                                setFieldValue(service, index, it.toString())
-//                                            }
-//                                        }
-//                                    }
-//                    paymentServiceFields.addView(view)
-//                }
-//                FieldType.REGEXBOX -> {
-//                    val view =
-//                            layoutInflater.inflate(R.layout.item_paynet_service_field_string, null)
-//                                    .apply {
-//                                        lblTitle.text =
-//                                                if (AppPrefs.language == UZ) serviceField.titleUz else serviceField.titleRu
-//                                        edtInput.doOnTextChanged { text, start, before, count ->
-//                                            text?.let {
-//                                                setFieldValue(service, index, it.toString())
-//                                            }
-//                                        }
-//                                    }
-//                    paymentServiceFields.addView(view)
-//                }
                 FieldType.NUMBER -> {
                     val view =
                         layoutInflater.inflate(R.layout.item_paynet_service_field_number, null)
@@ -304,14 +325,28 @@ class CreateRegularPaymentEnd @Inject constructor() :
     }
 
     private fun subscribeObservers() {
-
+        viewModel.savePaymentResp.observe(viewLifecycleOwner) {
+            when (it) {
+                is RequestState.Error -> {
+                    clTopUpLoading.isVisible = false
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
+                RequestState.Loading -> {
+                    lblSearching.text = getString(R.string.payment_creating)
+                    clTopUpLoading.isVisible = true
+                }
+                is RequestState.Success -> {
+                    lblTopUpSuccess.text = getString(R.string.payment_created)
+                    clTopUpLoading.isVisible = false
+                    clTopUpSuccess.isVisible = true
+                }
+            }
+        }
 
         viewModel.bftAndMyCardsPair.observe(viewLifecycleOwner) { requestState ->
             progressCards.isVisible = false
             setupCardsPager(requestState.first, requestState.second.getProperly())
         }
-
-
 
         viewModel.paynetServices.observe(viewLifecycleOwner) { response ->
             when (response) {
