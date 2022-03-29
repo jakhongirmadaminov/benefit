1e86e8bb47965a45b5c6b8cce43c0ca4f229c087
jmadaminov <aA1234567>
2022-03-29 12:34:21 +0500

    minor fixes

--- app/src/main/java/com/example/benefit/ui/auth/registration/RegistrationBSD.kt
+++ app/src/main/java/com/example/benefit/ui/auth/registration/RegistrationBSD.kt
@@ -1,25 +1,15 @@
 package com.example.benefit.ui.auth.registration
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.viewModels
 import com.example.benefit.R
-import com.example.benefit.ui.main.BenefitBSD
-
-import com.example.benefit.util.SizeUtils
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.benefit.ui.main.BenefitFixedHeightBSD
 
 
-
-class RegistrationBSD : BenefitBSD() {
+class RegistrationBSD : BenefitFixedHeightBSD() {
 
     private val viewModel: RegistrationViewModel by viewModels()
 
@@ -32,7 +22,6 @@ class RegistrationBSD : BenefitBSD() {
 
         return view
     }
-
 
 
 }
