8e32c9bf90dc77f5616f1dc3fdcc8cd5dde531e9
jahongir9779 <jahongir9779@gmail.com>
2022-04-15 17:52:11 +0500

    gfd

--- app/src/main/java/uz/magnumactive/benefit/ui/DialogLoading.kt
+++ app/src/main/java/uz/magnumactive/benefit/ui/DialogLoading.kt
@@ -1,6 +1,7 @@
 package uz.magnumactive.benefit.ui
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
