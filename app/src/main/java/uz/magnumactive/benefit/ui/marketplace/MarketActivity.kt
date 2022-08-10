6acd243c7ad028591dfa1d2b7c57cc4260099df9
jahongir9779 <jahongir9779@gmail.com>
2022-08-10 19:33:45 +0500

    minor fixes

--- app/src/main/java/uz/magnumactive/benefit/ui/marketplace/MarketActivity.kt
+++ app/src/main/java/uz/magnumactive/benefit/ui/marketplace/MarketActivity.kt
@@ -1,13 +1,21 @@
 package uz.magnumactive.benefit.ui.marketplace
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_gap_chart.*
 import kotlinx.android.synthetic.main.activity_market.*
+import kotlinx.android.synthetic.main.activity_market.tool_bar
 import uz.magnumactive.benefit.R
-import uz.magnumactive.benefit.ui.base.BaseActionbarActivity
 
-class MarketActivity : BaseActionbarActivity() {
+@AndroidEntryPoint
+class MarketActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +24,21 @@ class MarketActivity : BaseActionbarActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         nav_view.setupWithNavController(navController)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.elevation = 0F
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = Color.WHITE
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
