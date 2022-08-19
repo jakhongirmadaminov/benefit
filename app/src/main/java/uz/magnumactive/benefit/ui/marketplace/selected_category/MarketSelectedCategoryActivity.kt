0469ba350ebc8e2c4bd505e0b0cf2f8d8eb3ff31
jahongir9779 <jahongir9779@gmail.com>
2022-08-19 13:43:18 +0500

    minor fixes

---
+++ app/src/main/java/uz/magnumactive/benefit/ui/marketplace/selected_category/MarketSelectedCategoryActivity.kt
@@ -0,0 +1,20 @@
+package uz.magnumactive.benefit.ui.marketplace.selected_category
+
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gap_chart.*
+import uz.magnumactive.benefit.R
+import uz.magnumactive.benefit.ui.base.BaseActionbarActivity
+
+class MarketSelectedCategoryActivity : BaseActionbarActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_market_selected_category)
+        setSupportActionBar(tool_bar)
+        super.onCreate(savedInstanceState)
+//        tvTitle.text = intent.
+
+
+    }
+
+}
